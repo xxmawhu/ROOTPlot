72d9f335dd72366beeaa9ab8a4451ee7a5d22012
xxmawhu <xxmawhu@163.com>
2019-12-03 15:38:02 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ROOTPlot
+#ROOTPlot
 Some functions
--- inc/CalChiDistribution.hh
+++ inc/CalChiDistribution.hh
@@ -12,7 +12,7 @@
 #include <iostream>
 using namespace std;
 
-TGraphErrors CalChiDistribution(const TH1* hdata, const TH1 *hmc) {
+TGraphErrors CalChiDistribution(const TH1* hdata, const TH1* hmc) {
     // first histogram must be data, the last histogram is mc distribtuion
     // hdata and hmc must have same number of bins and bin size
     // the chi = (n_data - n_mc)/sigma
@@ -22,8 +22,8 @@ TGraphErrors CalChiDistribution(const TH1* hdata, const TH1 *hmc) {
     // check the bins
     if (hdata->GetNbinsX() != hmc->GetNbinsX()) {
         std::cout << "Error: the number of bins between data and MC is not equl"
-            << std::endl;
-        return TGraphErrors(0); 
+                  << std::endl;
+        return TGraphErrors(0);
     }
     // if one bin has less than 10 events, then merge this bin into next one.
     double xpos, ypos;
@@ -35,30 +35,31 @@ TGraphErrors CalChiDistribution(const TH1* hdata, const TH1 *hmc) {
     double yarroy[nbin];
     double errarroy[nbin];
     double errX[nbin];
-    int inx=0;
-    for (int i=1; i<=hdata->GetNbinsX(); ++i) {
-        double tmp_xpos=hdata->GetBinCenter(i);
+    int inx = 0;
+    for (int i = 1; i <= hdata->GetNbinsX(); ++i) {
+        double tmp_xpos = hdata->GetBinCenter(i);
         double tmp_ndata = hdata->GetBinContent(i);
         double tmp_nmc = hmc->GetBinContent(i);
         if (tmp_ndata < 0.1) {
             continue;
         }
-        xpos = (tmp_ndata*tmp_xpos + xpos * ndata)/ (ndata + tmp_ndata);
-        ndata  += tmp_ndata;
+        xpos = (tmp_ndata * tmp_xpos + xpos * ndata) / (ndata + tmp_ndata);
+        ndata += tmp_ndata;
         nmc += tmp_nmc;
         if (ndata < 10) {
             continue;
         }
         erry = sqrt(ndata);
-        ypos = (ndata - nmc)/erry;
-        xarroy[inx]  = xpos;
+        ypos = (ndata - nmc) / erry;
+        xarroy[inx] = xpos;
         yarroy[inx] = ypos;
         errarroy[inx] = 1.0;
         errX[inx] = 0.0;
-      //  cout << "index " << inx << endl;
-      //  cout << "x, y, err = " << xpos << ", " << ypos << ", " << erry << endl;
+        //  cout << "index " << inx << endl;
+        //  cout << "x, y, err = " << xpos << ", " << ypos << ", " << erry <<
+        //  endl;
         inx += 1;
-        ndata  = 0.0;
+        ndata = 0.0;
         nmc = 0.0;
         xpos = 0.0;
     }
@@ -66,9 +67,9 @@ TGraphErrors CalChiDistribution(const TH1* hdata, const TH1 *hmc) {
     cout << "xmax = " << hdata->GetXaxis()->GetXmax() << endl;
     cout << "xmin = " << hdata->GetXaxis()->GetXmin() << endl;
     graph.GetXaxis()->SetRangeUser(hdata->GetXaxis()->GetXmin(),
-            hdata->GetXaxis()->GetXmax());
+                                   hdata->GetXaxis()->GetXmax());
     graph.GetXaxis()->SetLimits(hdata->GetXaxis()->GetXmin(),
-            hdata->GetXaxis()->GetXmax());
+                                hdata->GetXaxis()->GetXmax());
     graph.GetYaxis()->SetLimits(-5, 5);
     graph.GetYaxis()->SetRangeUser(-5.1, 5.1);
     graph.GetYaxis()->SetNdivisions(20);
