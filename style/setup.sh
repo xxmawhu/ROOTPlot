0495c935a977d12dbd0eac25d81da931c0e9f091
xxmawhu <xxmawhu@163.com>
2019-12-14 17:49:24 +0800

    update

--- style/setup.sh
+++ style/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 PLOTSTYLE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sed "s:PATH_TO_THIS:${PLOTSTYLE}:"  ${PLOTSTYLE}/rootrc/rootlogon.C >~/.rootlogon.C 
+sed "s:PATH_TO_THIS:${PLOTSTYLE}/inc:"  ${PLOTSTYLE}/rootrc/rootlogon.C >~/.rootlogon.C 
