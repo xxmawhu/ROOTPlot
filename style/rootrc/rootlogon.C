8207afa4608de7698a6937fc4f25372c3fac8631
xxmawhu <xxmawhu@163.com>
2019-12-28 15:51:17 +0800

    update

--- style/rootrc/rootlogon.C
+++ style/rootrc/rootlogon.C
@@ -1,18 +1,15 @@
 {
-    printf("Welcome to the ROOT!\n");
+    // printf("Welcome to the ROOT!\n");
     gROOT->ProcessLine(".include PATH_TO_THIS");
     gStyle->SetTitleColor(0);
     gStyle->SetStatColor(0);
     gStyle->SetTitleFillColor(0);
-
-    // set the paper & margin sizes
-    // gStyle->SetPaperSize(20,26);
-    // gStyle->SetPadTopMargin(0.01);
-    // gStyle->SetPadRightMargin(0.12);
-    // gStyle->SetPadBottomMargin(0.13);
-    // gStyle->SetPadBottomMargin(0.04);
-
-    // gStyle->SetPadLeftMargin(0.15);
+    
+    // set the margin sizes
+    gStyle->SetPadTopMargin(0.06);
+    gStyle->SetPadRightMargin(0.06);
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadLeftMargin(0.18);
 
     // use large Times-Roman fonts
     gStyle->SetTitleFont(132, "xyz");   // set the all 3 axes title font
@@ -24,11 +21,6 @@
     gStyle->SetTextFont(132);
     gStyle->SetTextSize(0.08);
     gStyle->SetStatFont(132);
-
-    // use bold lines and markers
-    gStyle->SetMarkerStyle(8);
-    gStyle->SetHistLineWidth(2);
-    // gStyle->SetLineStyleString(2,"[12 12]"); // postscript dashes
 
     //..Get rid of X error bars
     gStyle->SetErrorX(0.001);
@@ -49,24 +41,6 @@
     gStyle->SetCanvasColor(0);
     gStyle->SetStatColor(0);
     gStyle->SetTitleFillColor(0);
-
-    // set the paper & margin sizes
-    //  gStyle->SetPaperSize(20,26);
-    //  gStyle->SetPadTopMargin(0.04);
-    //  gStyle->SetPadRightMargin(0.04);
-    //  gStyle->SetPadBottomMargin(0.15);
-    //  gStyle->SetPadLeftMargin(0.16);
-
-    // use large Times-Roman fonts
-    gStyle->SetTitleFont(132, "xyz");   // set the all 3 axes title font
-    gStyle->SetTitleFont(132, " ");     // set the pad title font
-    gStyle->SetTitleSize(0.06, "xyz");  // set the 3 axes title size
-    gStyle->SetTitleSize(0.06, " ");    // set the pad title size
-    gStyle->SetLabelFont(132, "xyz");
-    gStyle->SetLabelSize(0.05, "xyz");
-    gStyle->SetTextFont(132);
-    gStyle->SetTextSize(0.08);
-    gStyle->SetStatFont(132);
 
     // use bold lines and markers
     gStyle->SetMarkerStyle(8);
